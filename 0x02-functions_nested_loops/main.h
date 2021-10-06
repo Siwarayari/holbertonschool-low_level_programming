5a45d8cccdda258b94b14f2eaaf7953fd7b5323c
siwar9910 <91055926+siwar9910@users.noreply.github.com>
2021-10-06 13:08:25 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,8 +7,11 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int prvoid jack_bauer(void);
-int_last_digit(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 
 
 #endif
