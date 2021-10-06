50a27b6bb1ca68fa5542634dd31f6dbe39b558ea
root <siwarayari>
2021-10-06 01:07:38 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,16 +5,18 @@
  *
  */
 void print_alphabet_x10(void)
-{
-char ch;
+{       
+int ch;
 int i = 0;
 
-for (i = 0 ; i < 10 ; i++)
+  while ( i < 10 )
+
 {
 for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
 }
 putchar ('\n');
+i++;
 }
 }
