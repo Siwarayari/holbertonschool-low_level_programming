69710c3b28112f66b162d4c5ba110abc8bb33f38
root <siwarayari>
2021-10-06 01:02:43 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,12 +3,11 @@
  * print_alphabet_x10 - unction that prints 10 times the alphabet,
  * in lowercase, followed by a new line.
  *
- *Return: 0
  */
 void print_alphabet_x10(void)
 {
 char ch;
-int i;
+int i = 0;
 
 for (i = 0 ; i < 10 ; i++)
 {
