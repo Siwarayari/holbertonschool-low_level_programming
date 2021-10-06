8819441742002afa36aaad5965d9d6a7f681a271
siwar9910 <91055926+siwar9910@users.noreply.github.com>
2021-10-06 12:49:31 +0200

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -10,12 +10,12 @@
  */
 int _abs(int i)
 {
-        if (i >= 0)
-        {
-                return (i);
-        }
-        else
-        {
-                return (i * -1);
-        }
+if (i >= 0)
+{
+return (i);
+}
+else
+{
+return (i * -1);
+}
 }
