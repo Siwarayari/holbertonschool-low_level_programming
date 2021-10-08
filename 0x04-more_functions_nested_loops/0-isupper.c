b444dad7843b4c53325fb1c250b1bf53f7a1b10e
root <siwarayari>
2021-10-08 01:16:40 -0700

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	char t[] = "_putchar";
+
+	for (i = 0; i < 8 ; i++)
+		_putchar(t[i]);
+	_putchar('\n');
+	return (0);
+}
