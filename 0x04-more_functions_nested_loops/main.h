766c86c253f83a48d5013bbafca23fa72241813a
root <siwarayari>
2021-10-08 02:43:39 -0700

    task

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,10 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 int _isupper(int c);
-int _isdigit(int c);
-<<<<<<< HEAD
+int _isdigit(int c); HEAD
 int mul(int a, int b);
-=======
->>>>>>> e643c12fc122f682644e7a5f159210347a5c4da7
 
 #endif
