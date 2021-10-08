c225bff690cebcf02f4f7bf07b2d9bf6675bf2e9
root <siwarayari>
2021-10-08 05:27:15 -0700

    task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * more_numbers - Print numbers.
+ */
+void more_numbers(void);
+{
+int a, b;
+for (i = 1; i <= 10; i++)
+{
+for (j = 1; j <= 14; j++)
+_putchar(j + '0')
+_putchar('/n')
+}
+}
