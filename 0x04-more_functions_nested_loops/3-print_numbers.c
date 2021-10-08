e1424069b79395b7b1ef5a7e8109a17ef2895a6a
siwar9910 <91055926+siwar9910@users.noreply.github.com>
2021-10-08 11:31:07 +0100

    task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
- * print_numbers - get the signe of numbre
- */
+* print_numbers - description for this function
+* Return: Always 0 (Success)
+*/
 void print_numbers(void)
 {
 int i;
-for (i = 48; i < 58; i++)
-{
-_putchar(i);
-}
+for (i = 0; i <= 9; i++)
+_putchar('0' + i);
 _putchar('\n');
 }
