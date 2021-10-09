440a9f1523533dd9f03703dc4ae0370e4d47995f
siwar9910 <91055926+siwar9910@users.noreply.github.com>
2021-10-09 12:31:29 +0200

    task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - Print numbers.
+ * @n: int
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	if (n > 0)
+	_putchar('_');
+	}
+	_putchar('\n');
+}
