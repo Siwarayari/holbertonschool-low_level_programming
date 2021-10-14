3a25ccb0db412f2eeeba813eb2c8f53e316f88ae
siwar9910 <91055926+siwar9910@users.noreply.github.com>
2021-10-14 23:48:40 +0100

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,4 +5,6 @@ int main(void);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+intvoid reverse_array(int *a, int n); _strcmp(char *s1, char *s2);
+char *string_toupper(char *);
 #endif
