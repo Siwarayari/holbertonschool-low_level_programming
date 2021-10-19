6aae94ea48c4c4c359a738fb75ee7b01f7f2ce4b
root <siwarayari>
2021-10-19 04:26:39 -0700

    task1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,7 @@
  *@n: bytes from memory area.
  *Return: dest.
  */
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
