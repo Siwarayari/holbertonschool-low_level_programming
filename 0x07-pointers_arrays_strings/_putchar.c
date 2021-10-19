c3420d35a00b724ee4ffd7fb77eb9ad6cd5e475a
root <siwarayari>
2021-10-19 03:29:33 -0700

    putchar

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,18 +1,13 @@
-#include "main.h"
-/**
- * main - Task 0
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char ch[8] = "_putchar";
-	int i;
+#include <unistd.h>
 
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(ch[i]);
-	}
-	_putchar('\n');
-	return (0);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
