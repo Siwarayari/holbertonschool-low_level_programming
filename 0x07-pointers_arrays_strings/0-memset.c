5f757ce7c5551f746cc335f0ff04f2da512d941f
root <siwarayari>
2021-10-19 03:58:19 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,9 @@
-includ "main.h"
+#include "main.h"
 
 /**
- * _memset -fills the first n bytes
+ * _memset -fills the first n bytes of the
+ * area pointed to by s with b
+ *
  *@s: string to change
  *@b: constant byte
  *@n: number of b to change
@@ -13,7 +15,7 @@ char *_memset(char *s, char b, unsigned int n);
 {
 	unsigned int i;
 
-	for (i= 0; i <  n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
