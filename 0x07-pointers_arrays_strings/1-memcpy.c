ceca78e3bc12c3e6624c47388c3f9285bb9e8709
root <siwarayari>
2021-10-19 03:20:20 -0700

    task1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+ /**
+ *_memcpy - copies memory area.
+ *@dest: destination.
+ *@src: memory area.
+ *@n: bytes from memory area.
+ *Return: dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+
+	return (dest);
+}
