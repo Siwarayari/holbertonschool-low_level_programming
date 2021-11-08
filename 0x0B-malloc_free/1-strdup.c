607c13ba464af7a6c0731b28f5f95d1e0ba425ef
siwar9910 <91055926+siwar9910@users.noreply.github.com>
2021-11-09 00:05:26 +0100

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,60 +1,37 @@
-
-Pulls
-Issues
-Marketplace
-Explore
-@siwar9910
-Aymenbs22 /
-holbertonschool-low_level_programming
-Public
-
-1
-0
-
-    0
-
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-
-    Insights
-
-holbertonschool-low_level_programming/0x0B-malloc_free/1-strdup.c
-Aymenbs22 function that returns a pointer to a newly allocated space in memory
-Latest commit 185e294 13 days ago
-History
-0 contributors
-29 lines (28 sloc) 361 Bytes
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * _strdup - function that creates an array of chars
- * s: char
- * @str: char
- * Return: s
-*/
+ * _strdup - Returns a pointer to a newly-allocated space in memory
+ *           containing a copy of the string given as parameter.
+ * @str: The string to be copied.
+ *
+ * Return: If str == NULL or insufficient memory is available - NULL.
+ *         Otherwise - a pointer to the duplicated string.
+ */
+
 char *_strdup(char *str)
 {
-int i = 0;
-int j = 0;
-char *s;
-if (str == NULL)
-return (NULL);
+	char *sc;
+	int i, len = 0;
 
-while (str[i] != '\0')
-{
-i++;
-}
-s = malloc(sizeof(char) * i + 1);
-if (s == NULL)
-return (0);
-while (str[j] != '\0')
-{
-s[j] = str[j];
-j++;
-}
-return (s);
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		len++;
+
+	sc = malloc(sizeof(char) * (len + 1));
+
+	if (sc == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		sc[i] = str[i];
+
+	sc[len] = '\0';
+
+	return (sc);
+
+
 }
