19eda15b7f42c87f66fa989cb811c709b6b91786
root <siwarayari>
2021-11-17 12:52:47 -0800

    task8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_listint - function that returns the sum of all the data ,
+ * @head: head
+ * Return: sum
+*/
+int sum_listint(listint_t *head)
+{
+int sum = 0;
+
+	while (head)
+{
+	sum += head->n;
+	head = head->next;
+}
+	return (sum);
+}
