60ba93a16c3bcd2deb846f318d809ab615a44cee
root <siwarayari>
2021-11-17 12:35:59 -0800

    task4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_listint - function that frees a list
+ * @head: Head
+*/
+void free_listint(listint_t *head)
+{
+listint_t *tmp;
+while (head)
+{
+tmp = head->next;
+free(head);
+head = tmp;
+}
+}
