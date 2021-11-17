cc1c92b3a7ac687d0907d3cc19738a06c4e88811
root <siwarayari>
2021-11-17 13:13:17 -0800

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,65 +1,50 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * _listint_len - returns the number of elements in a linked listint_t list.
- * @h: linked list type listint_t
- * Return: the number of nodes
- */
-size_t _listint_len(const listint_t *h)
-{
-	unsigned int count = 0;
-
-	while (h)
-	{
-		count++;
-		h = h->next;
-	}
-
-	return (count);
-}
-
-/**
- * insert_nodeint_at_index - inserts a new node at a given position.
- * @head: linked lists
- * @idx: index to linked list
- * @n: number to linked list[index]
- * Return: node by index
- */
+  * insert_nodeint_at_index - ...
+  * @head: ...
+  * @idx: ...
+  * @n: ...
+  *
+  * Return: ...
+  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 0;
-	listint_t *node = *head, *newNode;
+	listint_t *new_node, *iter;
+	unsigned int count = 1;
 
-	if (!head || idx > _listint_len(*head))
-		return (NULL);
-
-	if (!idx)
+	if (head)
 	{
-		newNode = malloc(sizeof(listint_t));
-		if (!newNode)
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
 			return (NULL);
-		newNode->next = *head;
-		newNode->n = n;
-		*head = newNode;
-		return (newNode);
-	}
 
-	while (node)
-	{
-		if (i == (idx - 1))
+		new_node->n = n;
+		if (idx > 0)
 		{
-			newNode = malloc(sizeof(listint_t));
-			if (!newNode)
+			iter = *head;
+			while (iter)
+			{
+				if (count == idx)
+				{
+					new_node->next = iter->next;
+					iter->next = new_node;
+					return (new_node);
+				}
+				iter = iter->next;
+				count++;
+			}
+			if (idx > count)
 				return (NULL);
-
-			newNode->n = n;
-			newNode->next = node->next;
-			node->next = newNode;
-			return (newNode);
 		}
-		node = node->next;
-		i++;
+		else
+		{
+			new_node->next = *head;
+			*head = new_node;
+		}
+		return (new_node);
 	}
 	return (NULL);
 }
