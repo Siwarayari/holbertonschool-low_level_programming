32e0d7d3b720b16a52f06678387f5fee04a5adb5
siwar9910 <91055926+siwar9910@users.noreply.github.com>
2021-11-17 16:20:00 -0500

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,50 +1,51 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
-
 /**
-  * insert_nodeint_at_index - ...
-  * @head: ...
-  * @idx: ...
-  * @n: ...
-  *
-  * Return: ...
-  */
+ * listint_len - returns the number of elements in a linkedlisti\nt_t list
+* @h: a variable that points to a listint_t struct
+* Return: number of elements
+*/
+size_t listint_len(const listint_t *h)
+{
+	int counter = 0;
+
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
+/**
+ * insert_nodeint_at_index - inserts a new node at given position
+ * @head: pointer to pointer to struct
+ * @idx: index of list where new node should be added
+ * @n: value of n member for new node
+ *
+ * Return: adress of new node or NULL if it failed
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *iter;
-	unsigned int count = 1;
+	size_t length;
+	unsigned int i;
+	listint_t *new_node;
+	listint_t *prev_node = *head;
 
-	if (head)
-	{
+	length = listint_len(*head);
+		if (idx > length)
+			return (NULL);
 		new_node = malloc(sizeof(listint_t));
 		if (new_node == NULL)
 			return (NULL);
-
 		new_node->n = n;
-		if (idx > 0)
-		{
-			iter = *head;
-			while (iter)
-			{
-				if (count == idx)
-				{
-					new_node->next = iter->next;
-					iter->next = new_node;
-					return (new_node);
-				}
-				iter = iter->next;
-				count++;
-			}
-			if (idx > count)
-				return (NULL);
-		}
-		else
+		if (idx == 0)
 		{
 			new_node->next = *head;
 			*head = new_node;
+			return (new_node);
 		}
+		for (i = 0; i < idx - 1; i++)
+			prev_node = prev_node->next;
+		new_node->next = prev_node->next;
+		prev_node->next = new_node;
 		return (new_node);
-	}
-	return (NULL);
 }
