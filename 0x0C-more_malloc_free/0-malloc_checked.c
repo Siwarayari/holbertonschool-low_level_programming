9938fecc6ebbf6a9fea192f175c3791bf85e9fb2
siwar9910 <siwarayari1999@gmail.com>
2021-11-24 02:59:02 -0800

    task0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - llocates memory using malloc.
+ * @b: Size of memory
+ * Return: ptr.
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
