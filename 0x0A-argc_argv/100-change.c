ea29fd6dfb5d1901383603a9339228964f2e7b48
siwar9910 <siwarayari1999@gmail.com>
2021-11-26 02:07:25 -0800

    task5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,31 +1,53 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
-* main-program that prints the minimum number of coins to make
-*change for an amount of money
-*@argv:arguments
-*@argc:numbers of arguments
-*Return:Always success
-**/
+ * main - Entry point
+ * program that prints the minimum number
+ * of coins to make change for an amount of money.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 1 or 0
+ */
 int main(int argc, char *argv[])
 {
-int a, b = 0, i, tab[4] = {25, 10, 5, 2};
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
-a = atoi(argv[1]);
-if (a <= 0)
-printf("%d\n", 0);
-else
-{
-for (i = 0; i < 4; i++)
-{
-b += (a / tab[i]);
-a %= tab[i];
-}
-b += a;
-printf("%d\n", b);
-}
-return (0);
+	int a, change;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (atoi(argv[1]) <= 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	a = atoi(argv[1]);
+	change = 0;
+	while (a / 25 != 0)
+	{
+		change++;
+		a -= 25;
+	}
+	while (a / 10 != 0)
+	{
+		change++;
+		a -= 10;
+	}
+	while (a / 5 != 0)
+	{
+		change++;
+		a -= 5;
+	}
+	while (a / 2 != 0)
+	{
+		change++;
+		a -= 2;
+	}
+	if (a == 1)
+	{
+		change++;
+	}
+	printf("%d\n", change);
+	return (0);
 }
